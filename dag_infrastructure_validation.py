2bd795c71fbf8b6b440dd9fa788bdca973c7f8c5
Yusuf Ozdemir <115328146+OzdemirMirza@users.noreply.github.com>
2025-09-15 19:24:21 +0300

    Update dag_infrastructure_validation.py

--- dag_infrastructure_validation.py
+++ dag_infrastructure_validation.py
@@ -1,22 +1,21 @@
 import pendulum
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'max_active_runs': 1,
-    'retries': 3,
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "max_active_runs": 1,
+    "retries": 3,
 }
 
 with DAG(
     dag_id="spark_pi",
-    start_date=pendulum.datetime(2025, 9, 1, tz="UTC"),  # güncel bir tarih ver
-    schedule_interval="@daily",
+    start_date=pendulum.datetime(2025, 9, 1, tz="UTC"),  # başlangıç tarihi
+    schedule="@daily",  # <--- schedule_interval yerine schedule
     catchup=False,
     default_args=default_args,
     tags=["example"],
@@ -25,7 +24,7 @@ with DAG(
     submit = SparkKubernetesOperator(
         task_id="spark_transform_data",
         namespace="spark-operator",
-        application_file="/kubernetes/spark-pi.yaml",  # senin yaml path’in
+        application_file="/kubernetes/spark-pi.yaml",  # DAG klasöründeki yaml path’in
         kubernetes_conn_id="kubernetes_default",
         do_xcom_push=True,
     )
